0ca46e26d318413aff26208f08f159778bfa2f4d
samantonovich@gmail.com <samantonovich@gmail.com>
2022-01-31 15:15:33 +0300

    Add infrastructure project

---
+++ Octopus.Conductor.Infrastructure/Config/EntityDescriptionConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Octopus.Conductor.Core.Entities;
+
+namespace Octopus.Conductor.Infrastructure.Config
+{
+    public class EntityDescriptionConfig : IEntityTypeConfiguration<EntityDescription>
+    {
+        public void Configure(EntityTypeBuilder<EntityDescription> builder)
+        {
+            builder.HasKey(entity => entity.Id);
+            builder.Property(entity => entity.EntityType).IsRequired();
+            builder.Property(entity => entity.InputDirectory).IsRequired();
+            builder.Property(entity => entity.OutputDirectory).IsRequired();
+        }
+    }
+}
