fa977b57d1122fb5051a6b4a9765a4ba2afa513d
samantonovich@gmail.com <samantonovich@gmail.com>
2022-02-09 11:56:34 +0300

    Added custom exception for worker services's incorrect repeat interval

---
+++ src/Infrastructure/Octopus.Conductor.Infrastructure.WorkerService/Exceptions/IncorrectRepeatInterval.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Octopus.Conductor.Infrastructure.WorkerService.Exceptions
+{
+    public class IncorrectRepeatInterval : Exception
+    {
+        public IncorrectRepeatInterval(string msg): base(msg) { }
+    }
+}
