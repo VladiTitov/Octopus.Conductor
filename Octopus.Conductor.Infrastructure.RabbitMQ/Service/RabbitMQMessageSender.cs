08b2f69d4e4fcf62b1ed9e43096293ec9538ef82
samantonovich@gmail.com <samantonovich@gmail.com>
2022-02-16 15:47:51 +0300

    Added default implementations for rabbitmq interfaces

---
+++ Octopus.Conductor.Infrastructure.RabbitMQ/Service/RabbitMQMessageSender.cs
@@ -0,0 +1,17 @@
+﻿using Octopus.Conductor.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octopus.Conductor.Infrastructure.RabbitMQ.Service
+{
+    public class RabbitMQMessageSender : IMessageSender
+    {
+        public Task SendMessage(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Octopus.Conductor.Infrastructure.RabbitMQ/Service/RabbitMQPersistentConnection.cs
@@ -0,0 +1,26 @@
+﻿using Octopus.Conductor.Infrastructure.RabbitMQ.Interfaces;
+using RabbitMQ.Client;
+using System;
+
+namespace Octopus.Conductor.Infrastructure.RabbitMQ.Service
+{
+    public class RabbitMQPersistentConnection : IRabbitMQPersistentConnection
+    {
+        public bool IsConnected => throw new NotImplementedException();
+
+        public IModel CreateModel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryConnect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
