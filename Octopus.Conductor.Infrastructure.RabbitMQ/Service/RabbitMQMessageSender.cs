6b3b59ecc31f32b01c933db9846593f87a5c525c
samantonovich@gmail.com <samantonovich@gmail.com>
2022-02-17 17:54:11 +0300

    Added channel creating

--- Octopus.Conductor.Infrastructure.RabbitMQ/Service/RabbitMQMessageSender.cs
+++ Octopus.Conductor.Infrastructure.RabbitMQ/Service/RabbitMQMessageSender.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Octopus.Conductor.Infrastructure.RabbitMQ.Service
@@ -19,6 +20,8 @@ namespace Octopus.Conductor.Infrastructure.RabbitMQ.Service
         private readonly IRabbitMQPersistentConnection _persistentConnection;
         private readonly ILogger<RabbitMQMessageSender> _logger;
         private readonly int _retryCount;
+        private IModel _channel;
+        private Policy _policy;
 
         public RabbitMQMessageSender(IRabbitMQPersistentConnection persistentConnection,
             ILogger<RabbitMQMessageSender> logger,
@@ -27,20 +30,42 @@ namespace Octopus.Conductor.Infrastructure.RabbitMQ.Service
             _persistentConnection = persistentConnection;
             _logger = logger;
             _retryCount = retryCount;
+            _policy = CreatePolicy();
         }
+
+        private Policy CreatePolicy() =>
+            RetryPolicy.Handle<BrokerUnreachableException>()
+                .Or<SocketException>()
+                .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+                    {
+                        _logger.LogWarning(ex, "Could not publish message after {Timeout}s ({ExceptionMessage})", $"{time.TotalSeconds:n1}", ex.Message);
+                    });
+
         public Task SendMessage(object message)
         {
             if (!_persistentConnection.IsConnected)
             {
-                _persistentConnection.TryConnect();
+                if (_persistentConnection.TryConnect())
+                    _channel = _persistentConnection.CreateModel();
             }
+            
+            var body = JsonSerializer.SerializeToUtf8Bytes(message, message.GetType());
 
-            var policy = RetryPolicy.Handle<BrokerUnreachableException>()
-            .Or<SocketException>()
-            .WaitAndRetry(_retryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+            _policy.Execute(() =>
             {
-                _logger.LogWarning(ex, "Could not publish event: {EventId} after {Timeout}s ({ExceptionMessage})", @event.Id, $"{time.TotalSeconds:n1}", ex.Message);
+                var properties = _channel.CreateBasicProperties();
+                IModel
+                properties.DeliveryMode = 2;
+
+                _channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "",
+                    mandatory: true,
+                    basicProperties: properties,
+                    body: body);
             });
+
+            return Task.CompletedTask;
         }
 
         private IModel CreatePublisherChannel()
