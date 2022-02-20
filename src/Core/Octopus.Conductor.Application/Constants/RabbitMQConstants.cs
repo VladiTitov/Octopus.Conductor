3a8ff296994afbb9aaa63bee6b7f9388dc55ae43
samantonovich@gmail.com <samantonovich@gmail.com>
2022-02-20 21:47:21 +0300

    Added constants for rabbitmq

---
+++ src/Core/Octopus.Conductor.Application/Constants/RabbitMQConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octopus.Conductor.Application.Constants
+{
+    public static class RabbitMQConstants
+    {
+        public const int RetryCount = 5;
+        
+        public const string FolderListnerChannelName = "demo-channel";
+
+        public const string FolderListnerExchangeName = "demo-exchange";
+
+        public const string FolderListnerRoutingKey = "demo-key";
+    }
+}
