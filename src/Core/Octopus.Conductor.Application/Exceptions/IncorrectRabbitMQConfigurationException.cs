155d6d535f7d9378cde9896f1b1878dbaef08af8
samantonovich@gmail.com <samantonovich@gmail.com>
2022-02-21 15:02:34 +0300

    Added custom exception for incorrect rabbit's config

---
+++ src/Core/Octopus.Conductor.Application/Exceptions/IncorrectRabbitMQConfigurationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octopus.Conductor.Application.Exceptions
+{
+    public class IncorrectRabbitMQConfigurationException : Exception
+    {
+        public IncorrectRabbitMQConfigurationException(string msg)
+            : base(msg) { }
+    }
+}
