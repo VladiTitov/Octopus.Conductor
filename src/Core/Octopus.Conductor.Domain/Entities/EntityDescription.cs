fbbdd37f43502e64f44435bd79b0d5405e47ec33
samantonovich@gmail.com <samantonovich@gmail.com>
2022-03-21 11:05:51 +0300

    Added entitydescription model

---
+++ src/Core/Octopus.Conductor.Domain/Entities/EntityDescription.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Octopus.Conductor.Domain.Entities
+{
+    public class EntityDescription
+    {
+        public string EntityType { get; set; }
+        public string EntityFilePath { get; set; }
+    }
+}
